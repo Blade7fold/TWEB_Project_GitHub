7f5647f950897deade8872f7e71ceea7521320fe
jimmyVerdasca <jimmy.verdasca@heig-vd.ch>
2018-10-07 18:57:12 +0200

    init server

---
+++ server/githubServer.js
@@ -0,0 +1,51 @@
+const fetch = require('node-fetch');
+
+class ResponseError extends Error {
+  constructor(res, body) {
+    super(`${res.status} error requesting ${res.url}: ${res.statusText}`);
+    this.status = res.status;
+    this.path = res.url;
+    this.body = body;
+  }
+}
+
+class Github {
+  constructor({ token, baseUrl = 'https://api.github.com' } = {}) {
+    this.token = token;
+    console.log(this.token)
+    this.baseUrl = baseUrl;
+  }
+
+  setToken(token) {
+    this.token = token;
+  }
+
+  request(path, opts = {}) {
+    const url = `${this.baseUrl}${path}`;
+    const options = {
+      ...opts,
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token ${this.token}`,
+      },
+    };
+    console.log(url)
+    console.log(options)
+    return fetch(url, options)
+      .then(res => res.json()
+        .then((data) => {
+          if (!res.ok) {
+            throw new ResponseError(res, data);
+          }
+
+          return data;
+        }));
+  }
+
+  users(since) {
+    return this.request('/users',{'since':since});
+  }
+}
+let git = new Github({token:'3ba3eb13c5717b8090c4195ac0da13eb965195fe'})
+git.users('135').then(result => console.log(result))
+module.exports = Github;
