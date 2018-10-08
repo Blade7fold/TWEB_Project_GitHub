554faf8daf97a739532aa5bf04f00939d14c927d
BLADE7FOLD\Nathan <nathan.gonzalezmontes@heig-vd.ch>
2018-10-08 16:56:50 +0200

    Buttons scroll down after clickig them

--- web/js/index.js
+++ web/js/index.js
@@ -1,8 +1,24 @@
-let i = 0;
+buttonArray = ['.commit', '.git_race', '.twinder', '.back']
 
-function followUp() {
-  i = parseInt(document.getElementById("followers").innerText);
-  i = i + 1;
-  document.getElementById("followers").innerHTML = i;
-  alert('Thanks for the follow!');
+for(let i = 0; i < buttonArray.length; i++) {
+  $(document).ready(function() {
+    $(buttonArray[i]).on('click', function() { // Au clic sur un bouton
+      var page = $(this).attr('href'); // Endroit ciblé
+      var speed = 750; // Durée de l'animation (en ms)
+      $('html, body').animate( { scrollTop: $(page).offset().top }, speed);
+    });
+  });
 }
+
+// let i = 0;
+// 
+// function followUp() {
+//   i = parseInt(document.getElementById("followers").innerText);
+//   i = i + 1;
+//   document.getElementById("followers").innerHTML = i;
+//   alert('Thanks for the follow!');
+// }
+// 
+// function pushed() {
+//   alert('Pushed the button!');
+// }
