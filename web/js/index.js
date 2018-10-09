cca2d99eb99e2d3088d110ff2a0f9767ff6e9940
BLADE7FOLD\Nathan <nathan.gonzalezmontes@heig-vd.ch>
2018-10-09 10:24:38 +0200

    Putting text in bold font

--- web/js/index.js
+++ web/js/index.js
@@ -16,7 +16,6 @@ for(let i = 0; i < buttonArray.length; i++) {
 //   i = parseInt(document.getElementById("followers").innerText);
 //   i = i + 1;
 //   document.getElementById("followers").innerHTML = i;
-//   alert('Thanks for the follow!');
 // }
 // 
 // function pushed() {
