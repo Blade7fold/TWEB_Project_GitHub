bf2cf8a391717f98661552d2ff5897931e58a774
BLADE7FOLD\Nathan <nathan.gonzalezmontes@heig-vd.ch>
2018-10-12 09:01:54 +0200

    Tried to insert template in project, but now trying the opposite

--- web/js/index.js
+++ web/js/index.js
@@ -1,3 +1,6 @@
+
+$WIN = $(window);
+
 buttonArray = ['.commit_btn', '.git_race_btn', '.twinder_btn', '.back_btn']
 
 for(let i = 0; i < buttonArray.length; i++) {
@@ -9,6 +12,29 @@ for(let i = 0; i < buttonArray.length; i++) {
     });
   });
 }
+
+var clBackToTop = function() {
+        
+  var pxShow  = 500,         // height on which the button will show
+  fadeInTime  = 400,         // how slow/fast you want the button to show
+  fadeOutTime = 400,         // how slow/fast you want the button to hide
+  scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+  goTopButton = $(".go-top")
+  
+  // Show or hide the sticky footer button
+  $(window).on('scroll', function() {
+      if ($(window).scrollTop() >= pxShow) {
+          goTopButton.fadeIn(fadeInTime);
+      } else {
+          goTopButton.fadeOut(fadeOutTime);
+      }
+  });
+};
+
+(function ssInit() {
+  clSmoothScroll();
+  clBackToTop();
+})();
 
 // let i = 0;
 // 
