6fb9170d544be83ba6a58f1f00d11276e5f49ca9
BLADE7FOLD\Nathan <nathan.gonzalezmontes@heig-vd.ch>
2018-10-19 21:25:26 +0200

    Changing project and separating server and client

--- server/httpServer.js
+++ server/httpServer.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
 var github = require('./githubServer')
-require('dotenv').config();
+
 
 let git = new github(process.env.TOKEN)
 
+console.log('TOKEN', process.env.TOKEN);
 //git.usernames(200).then(result => console.log(result))
 //git.stats(200).then(result => console.log(result))
+console.log('Connected');
 
 http.createServer(function (req, res) {
+    console.log('Request recieved')
     let q = url.parse(req.url, true)
+    console.log(req.url)
 
     if (q.pathname == "/commit") {
         git.commitsOf(q.query['user'])
@@ -31,6 +36,8 @@ http.createServer(function (req, res) {
             } catch(err) {
                 console.log(err)
             }
+        }).catch(err => {
+            console.log(err)
         })
     } else if (q.pathname == "/stat") {
         git.stats(q.query['seed'])
--- web/js/index.js
+++ web/js/index.js
@@ -1,11 +1,33 @@
 
-$WIN = $(window);
+// $WIN = $(window);
+
+function getUserFollowers() {
+  return fetch(`localhost:8080/follower?seed=1234`)
+    .then(response => {
+    if (!response.ok) throw new Error('Not found!');
+      return response.json();
+    })
+    .then(user => user.follower_url);
+}
+// var express = require('express');
+// var app = express();
 
 buttonArray = ['.commit_btn', '.git_race_btn', '.twinder_btn', '.back_btn']
 
 for(let i = 0; i < buttonArray.length; i++) {
   $(document).ready(function() {
     $(buttonArray[i]).on('click', function() { // Au clic sur un bouton
+      getUserFollowers()
+        .then(follower_url => {
+          const img = document.createElement('img');
+          img.src = follower_url;
+          document.body.append(img);
+        })
+        .catch(err => {
+          const p = document.createElement('p');
+          p.innerText = 'Sorry';
+          document.body.append(p);
+      })
       var page = $(this).attr('href'); // Endroit ciblé
       var speed = 750; // Durée de l'animation (en ms)
       $('html, body').animate( { scrollTop: $(page).offset().top }, speed);
@@ -13,28 +35,35 @@ for(let i = 0; i < buttonArray.length; i++) {
   });
 }
 
-var clBackToTop = function() {
-        
-  var pxShow  = 500,         // height on which the button will show
-  fadeInTime  = 400,         // how slow/fast you want the button to show
-  fadeOutTime = 400,         // how slow/fast you want the button to hide
-  scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
-  goTopButton = $(".go-top")
-  
-  // Show or hide the sticky footer button
-  $(window).on('scroll', function() {
-      if ($(window).scrollTop() >= pxShow) {
-          goTopButton.fadeIn(fadeInTime);
-      } else {
-          goTopButton.fadeOut(fadeOutTime);
-      }
-  });
-};
 
-(function ssInit() {
-  clSmoothScroll();
-  clBackToTop();
-})();
+
+// respond with "hello world" when a GET request is made to the homepage
+// app.get('/follower?seed=100', function(req, res) {
+//   console.log('wesh');
+// });
+
+// var clBackToTop = function() {
+        
+//   var pxShow  = 500,         // height on which the button will show
+//   fadeInTime  = 400,         // how slow/fast you want the button to show
+//   fadeOutTime = 400,         // how slow/fast you want the button to hide
+//   scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+//   goTopButton = $(".go-top")
+  
+//   // Show or hide the sticky footer button
+//   $(window).on('scroll', function() {
+//       if ($(window).scrollTop() >= pxShow) {
+//           goTopButton.fadeIn(fadeInTime);
+//       } else {
+//           goTopButton.fadeOut(fadeOutTime);
+//       }
+//   });
+// };
+
+// (function ssInit() {
+//   clSmoothScroll();
+//   clBackToTop();
+// })();
 
 // let i = 0;
 // 
