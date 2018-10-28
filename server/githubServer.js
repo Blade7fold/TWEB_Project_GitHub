b2add10266497dab57ad4a407257a179395ffc2e
jimmyVerdasca <jimmy.verdasca@heig-vd.ch>
2018-10-28 13:58:49 +0100

    server cleaned

--- server/githubServer.js
+++ server/githubServer.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
-var utils = require('./utils')
+var utils = require('./utils');
 var parse = require('parse-link-header');
-let util = new utils()
+let util = new utils();
 
 /**
  * response class sent when a fetch fail
@@ -17,6 +17,9 @@ class ResponseError extends Error {
 
 /**
  * class managing the connexion to the github API.
+ * 
+ * This class could be improve with a cache that store usefull
+ * information instead of asking at every request to github API rest.
  */
 class Github {
   constructor(token, baseUrl = 'https://api.github.com') {
@@ -28,6 +31,7 @@ class Github {
    * method sending a request to the given path url. and return the body datas.
    * @param {url of the request} path 
    * @param {dictionary used by github to navigate through big data answers} search_opt 
+   * @param {boolean indicating that we want only the headers of the response} onlyHeaders 
    * @param {dictionnary to add specific headers to the request} opts 
    */
   request(path, search_opt = {}, onlyHeaders = false, opts = {}) {
@@ -39,7 +43,7 @@ class Github {
         Authorization: `token ${this.token}`
       },
     };
-    console.log('URL: ' + url)
+    
     return fetch(url, options)
       .then(res => {
         return res.json()
@@ -47,20 +51,21 @@ class Github {
             if (!res.ok) {
               throw new ResponseError(res, data);
             }
-            if(onlyHeaders) {
-              return res.headers
+            if (onlyHeaders) {
+              return res.headers;
             }
             return data;
           })
-      })
+      });
   }
 
   /**
-   * method sending a request to get the users informations from github
+   * Method sending a request to get the users informations from github
    * @param {seed of the first user we want} since 
    */
   users(since) {
-    return this.request('/users', {'since':since,'page':1,'per_page':5});
+    let nb_users_wanted = 5; // Minimum: 1, Maximum: 100 We could improve by taking it from the client request
+    return this.request('/users', {'since':since,'page':1,'per_page':nb_users_wanted});
   }
 
   /**
@@ -75,14 +80,14 @@ class Github {
       .then(infos_users => {
         let infos = [];
         for (let i = 0; i < infos_users.length; i++) {
-          let user_info = {}
+          let user_info = {};
           for (let j = 0; j < info.length; j++) {
-            user_info[info[j]] = infos_users[i][info[j]]
+            user_info[info[j]] = infos_users[i][info[j]];
           }
-          infos.push(user_info)
+          infos.push(user_info);
         }
-        return infos
-      })
+        return infos;
+      });
   }
 
   /**
@@ -95,42 +100,48 @@ class Github {
   followers(since) {
     return this.users(since).then((function(infos_users) {
       let cleaned_infos_users = [];
-      let cleaned_user = {}
-      let url_followers = ''
-      let followers_promises = []
+      let cleaned_user = {};
+      let url_follower = '';
+      let followers_promises = [];
+
+      //Ask to github the followers of the current iterated user and put the promise in followers_promises
       for (let i = 0; i < infos_users.length; i++) {
-        let user = infos_users[i]
-        url_followers = user['followers_url'].replace(this.baseUrl,'')
-        followers_promises.push(this.request(url_followers, {"per_page": 1}, true))
+        let user = infos_users[i];
+        url_follower = user['followers_url'].replace(this.baseUrl,'');
+        followers_promises.push(this.request(url_follower, {"per_page": 1}, true));
+
+        // Add the username, the image avatar url and the id to the final answer
         cleaned_user = {'login': user['login'],
                         'avatar_url': user['avatar_url'],
                         'id': user['id']
-                      }
-        cleaned_infos_users.push(cleaned_user)
+                      };
+        cleaned_infos_users.push(cleaned_user);
       }
+
+      // Wait followers promises to be resolved before calculating the number of followers of the users
       return Promise.all(followers_promises).then(list_followers => {
         for (let i = 0; i < list_followers.length; i++) {
-          if(parse(list_followers[i].get('Link')) !== null) {
-            cleaned_infos_users[i]['nb_followers'] = parse(list_followers[i].get('Link'))['last']['page']
-          }else {
+          if (parse(list_followers[i].get('Link')) !== null) {
+            cleaned_infos_users[i]['nb_followers'] = parse(list_followers[i].get('Link'))['last']['page'];
+          } else {
             cleaned_infos_users[i]['nb_followers'] = 0;
           }
         }
-        return cleaned_infos_users
-      })
-    }).bind(this))
+        return cleaned_infos_users;
+      });
+    }).bind(this));
     
   }
 
   /**
-   * request to github the number of repositories of a given user
+   * Request to github the number of repositories of a given user.
    * @param {username of the user we want the number of repositories} user 
+   * Return 0 if the request fail or the answer is empty.
    */
   nbRepositoriesOf(user) {
     return this.repositoriesOf(user)
       .then(repos => {
         if(repos === undefined) {
-          console.log("repos undefined")
           return 0;
         }
         return repos['total_count']
@@ -138,30 +149,38 @@ class Github {
   }
 
   /**
-   * request to github the repositories of a given user (fork are considered aswell)
+   * Request to github the repositories of a given user (fork are considered aswell).
    * @param {username of the user we want the repositories} user 
+   * Return undefined if the request fails (happen when repeateadly ask the repositories of a private user)
    */
   repositoriesOf(user) {
-    return this.request('/search/repositories', {'q':util.dictToSearchOption({'user':user, 'fork': 'true'})}).catch(err => {console.log("fuck6" + err.status);return undefined})
+    let search_options = {'q':util.dictToSearchOption({'user':user, 'fork': 'true'})};
+    return this.request('/search/repositories', search_options)
+      .catch(err => {
+        return undefined;
+      });
   }
 
   /**
-   * Use a recursive strategy to request the all the commits message of a given user.
+   * Use a recursive strategy to request all the commits message of a given user.
    * @param {username of the user we want the commits} user 
    * @param {number of the page the where the recursive call is} page 
    * @param {list of the commits message currently found} commit_msg 
    */
   commitsOf(user, page = 1, commit_msg = []) {
-    return this.request('/search/commits', {'page':page++, 'per_page': 100, 'q':util.dictToSearchOption({'author':user})}).catch(err => {console.log("fuck7"); return ''})
-      .then(commits => {
-        if (commits) {
-          for (let i = 0; i < commits['items'].length; i++) {
-            commit_msg.push(commits['items'][i]['commit']['message'])
-          } 
-          return this.commitsOf(user, page, commit_msg)
-        }
-        return commit_msg
-      }, error => {return commit_msg}).catch(err => {console.log("fuck20"); return ''})
+    let search_options = {'page':page++, 'per_page': 100, 'q':util.dictToSearchOption({'author':user})};
+    return this.request('/search/commits', search_options)
+        .then(commits => {
+          if (commits) {
+            for (let i = 0; i < commits['items'].length; i++) {
+              commit_msg.push(commits['items'][i]['commit']['message']);
+            } 
+            return this.commitsOf(user, page, commit_msg); // recursive call to next page
+          }
+          return commit_msg; // No more pages we can return the commit list
+        }, error => {
+          return commit_msg; // If a commit request fail we don't consider this page and return the current list
+        });
   }
 
   /**
@@ -169,85 +188,73 @@ class Github {
    * @param {username of the user we want the number of commits} user 
    */
   nbCommitsOf(user) {
-    return this.request('/search/commits', {'q':util.dictToSearchOption({'author':user})})
+    let search_options = {'q':util.dictToSearchOption({'author':user})};
+    return this.request('/search/commits', search_options)
       .catch(err => {
-        console.log("fuck8" + err);
-        return {'total_count': 0}
+        return {'total_count': 0};
       })
       .then(commits => {
-        return commits['total_count']
-      })
+        return commits['total_count'];
+      });
   }
 
   /**
    * request to github the lines code of a user and calculate their total number to return it.
+   * Github return the list of weekly lines of code added. We need to iterate through it.
    * @param {username of the user we want the number of line code} user 
    */
-  linesOf(user) {
-    let nb_lines = 0
-    let forbidden = false
+  nbLinesOf(user) {
+    let nb_lines = 0;
     return this.repositoriesOf(user)
       .catch(err => {
-        console.log("catch" + err);
-        if(err.status === 403) {
-          forbidden = true
-        }
-        return undefined
+        return undefined;
       })
       .then(repos_data => {
-        if(repos_data === undefined || forbidden) {
-          console.log("repos pas trouvé")
+        if(repos_data === undefined) {
           return 0;
         }
-        let per_page = 30
-        let nb_repos = repos_data['total_count']
-        let repos = repos_data['items']
-        let names_repos = []
+        let per_page = 30;
+        let nb_repos = repos_data['total_count'];
+        let repos = repos_data['items'];
+        let names_repos = [];
+
+        // foreach repositories
         if(repos != []) {
-          for (let i = 0; i < nb_repos && !forbidden; i++) {
+          for (let i = 0; i < nb_repos; i++) {
             let page = Math.ceil((i  + 1) / per_page)
             let index_on_page = i - (page - 1) * per_page
             if(repos[index_on_page] !== undefined) {
+
+              // We ask the contributions of a user for this repos
                names_repos.push(this.request(`/repos/${repos[index_on_page]['full_name']}/stats/contributors`, {'page':page})
                 .catch(err => {
-                  console.log("fuck10" + err);
-                  if(err.status === 403) {
-                    console.log("STOP FORBIDDEN")
-                    forbidden = true
-                  }
-                  return undefined
+                  return undefined;
                 })
                 .then(contributions_data => {
+                  // Then we add weekly number of lines
                   if (contributions_data !== undefined) {
                     for (let i = 0; i < contributions_data.length; i++) {
-                      if (contributions_data[i] !== undefined) {
-                        for (let k = 0; k < contributions_data[i]['weeks'].length; k++) {
-                          nb_lines += contributions_data[i]['weeks'][k]['a']
-                        }
+                      for (let k = 0; k < contributions_data[i]['weeks'].length; k++) {
+                        nb_lines += contributions_data[i]['weeks'][k]['a']
                       }
                     }
                   }
                   return nb_lines
-                })
-                .catch(err =>  {
-                  console.log("fuck9");
+              }));
+              Promise.all(names_repos.slice(names_repos.lenght, names_repos.length + 1))
+                .then(result => {
                   return nb_lines;
-                })
-              )
+                });
             } 
           }
         } 
-        return Promise.all(names_repos).then(result => {return nb_lines;}).catch(err => {
-          console.log("fuck11" + err);return nb_lines})
-          
-      }).catch(err => { 
-        console.log("fuck10");
-        if (nb_lines !== undefined) {
-          return nb_lines
-        } else {
-          return 0
-        }
+
+        // We wait for all promise to finish to return the number of total lines of the user
+        return Promise.all(names_repos)
+        .then(result => {
+          return nb_lines;
         })
+      })
   }
 
   /**
@@ -263,37 +270,29 @@ class Github {
   stats(since) {
     return this.userInfo(since, ['login', 'avatar_url'])
       .then(name_and_avatar => {
-
-        /*
-        if (name_and_avatar === undefined) {
-          return {'username': "private",
-                  'avatar_url': undefined,
-                  'nb_lines': 0,
-                  'nb_commit': 0,
-                  'nb_repos': 0
-                  }
-        }*/
-
-        let statistics = []
+        let statistics = [];
         for (let i = 0; i < name_and_avatar.length; i++) {
-          let name = name_and_avatar[i]['login']
-          let avatar = name_and_avatar[i]['avatar_url']
-          statistics.push(Promise.all([this.linesOf(name), this.nbCommitsOf(name), this.nbRepositoriesOf(name)])
+          let name = name_and_avatar[i]['login'];
+          let avatar = name_and_avatar[i]['avatar_url'];
+          statistics.push(Promise.all([this.nbLinesOf(name), this.nbCommitsOf(name), this.nbRepositoriesOf(name)])
           .then(result => {
             return {'username': name,
                     'avatar_url': avatar,
                     'nb_lines': result[0],
                     'nb_commit': result[1],
                     'nb_repos': result[2]
-                    }
+                    };
           
-          }))
+          }));
         }
-        return Promise.all(statistics).catch(err => console.log("plop" + err)).then(result => {return result})
+        return Promise.all(statistics)
+        .then(result => {
+            return result;
+          });
       }
     ).catch(err => {
       console.log(err);
-      })
+    });
   }
 }
 
--- server/httpServer.js
+++ server/httpServer.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
-var http = require('http');
 var express = require('express');
 var app = express();
 var url = require('url');
-var querystring = require('querystring');
-var github = require('./githubServer')
+var github = require('./githubServer');
 
 
-let git = new github(process.env.TOKEN)
+let git = new github(process.env.TOKEN);
 
 console.log('Connected');
 
@@ -16,19 +14,20 @@ console.log('Connected');
  * return to the client the formatted answer
  */
 app.get('/commit', function(req, res) {
-    let q = url.parse(req.url, true)
-    console.log("URL HTTP: " + q)
-    git.commitsOf(q.query['user'])
-    .then(data => {
-        try {
-            console.log("USER HTTP: " + q.query['user'])
-            console.log("DATA: " + data)
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.json(data)
-        } catch(err) {
-            console.log(err)
-        }
-    })
+    let q = url.parse(req.url, true);
+    let user = q.query['user'];
+    console.log("asking to github commits of : " + user);
+
+    git.commitsOf(user)
+        .then(data => {
+            try {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.json(data);
+                console.log("commits sent back to client");
+            } catch(err) {
+                console.log(err);
+            }
+        });
 });
   
 /**
@@ -39,14 +38,18 @@ app.get('/commit', function(req, res) {
  * return to the client the formatted answer
  */
 app.get('/follower', function(req, res) {
-    let q = url.parse(req.url, true)
-    git.followers(q.query['seed'])
+    let q = url.parse(req.url, true);
+    let seed = q.query['seed'];
+    console.log("asking to github users with starting seed : " + seed);
+
+    git.followers(seed)
     .then(data => {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.json(data)
+        res.json(data);
+        console.log("users sent back to client");
     }).catch(err => {
-        console.log(err)
-    })
+        console.log(err);
+    });
 });
 
 /**
@@ -57,15 +60,18 @@ app.get('/follower', function(req, res) {
  * return to the client the formatted answer
  */
 app.get('/stat', function(req, res) {
-    let q = url.parse(req.url, true)
-    git.stats(q.query['seed'])
+    let q = url.parse(req.url, true);
+    let seed = q.query['seed'];
+    console.log("asking to github statistics with starting seed : " + seed);
+
+    git.stats(seed)
     .then(data => {
-        console.log(data)
         try {
             res.setHeader('Access-Control-Allow-Origin', '*');
-            res.json(data)
+            res.json(data);
+            console.log("statistics sent back to client");
         } catch(err) {
-            console.log(err)
+            console.log(err);
         }
     })
 });
@@ -75,7 +81,10 @@ app.get('/stat', function(req, res) {
  * we send back to the client a 404 not found.
  */
 app.get('*', function(req, res){
-    res.status(404).send("pathname not found")
+    let q = url.parse(req.url, true);
+    console.log(q + " pathname not found");
+    res.status(404).send(q + " pathname not found");
+    console.log("404 sent to client");
 });
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
